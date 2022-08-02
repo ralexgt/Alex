ee2cca89090b88e98a1f111713e52c14c0efd5fa
Alex <romanalexrares@gmail.com>
2022-08-02 12:16:48 +0300

    ip location

---
+++ ip-country/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 5000
+
+
+let location = {
+    ip: "ip",
+    continent: "continent",
+    country: "country"
+}
+
+app.use(bodyParser.json());
+const maxmind = require('maxmind');
+/*app.post("/:ip", function (req, res) {
+    maxmind.open('GeoLite2-City_20220802/GeoLite2-City.mmdb').then((lookup) => {
+        let myip = req.params.ip
+        if(maxmind.validate(myip)){
+            location.ip = myip
+            location.continent = lookup.get(myip).continent.names.en
+            location.country = lookup.get(myip).country.names.en
+            res.send(location) 
+        }
+        else {
+            res.send(`invalid IP`)
+        }
+        //res.send(`IP: ${myip} \n Continent: ${lookup.get(myip).continent.names.en} \n Country: ${lookup.get(myip).country.names.en}`)
+    });    
+})*/
+app.post("/", function (req, res) {
+    maxmind.open('GeoLite2-City_20220802/GeoLite2-City.mmdb').then((lookup) => {
+        let myip = req.body.ip
+        if(maxmind.validate(myip)){
+            location.ip = myip
+            location.continent = lookup.get(myip).continent.names.en
+            location.country = lookup.get(myip).country.names.en
+            res.send(location) 
+        }
+        else {
+            res.send(`invalid IP`)
+        }
+        //res.send(`IP: ${myip} \n Continent: ${lookup.get(myip).continent.names.en} \n Country: ${lookup.get(myip).country.names.en}`)
+    });    
+})
+
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
