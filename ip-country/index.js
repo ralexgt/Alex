d4e047fd19b42b5334c5fe2ac355ebaa5c469447
Ciobanu Laurentiu <laurentiu.ciobanu@flowos.com>
2022-08-03 12:03:16 +0300

    code review fixes

--- ip-country/index.js
+++ ip-country/index.js
@@ -1,59 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const maxmind = require('maxmind');
+const express = require("express");
+const bodyParser = require("body-parser");
+const maxmind = require("maxmind");
 
-const app = express()
-const PORT = 5000
+const PORT = 5000;
+const DB_PATH = "GeoLite2-City_20220802/GeoLite2-City.mmdb";
 
-let location = {
-    ip: "ip",
-    continent: "continent",
-    country: "country"
+async function main() {
+  const lookup = await maxmind.open(DB_PATH);
+
+  const app = express();
+
+  app.use(bodyParser.json());
+
+  app.post("/api/location", function (req, res) {
+    let clientIp = req.body.ip;
+
+    if (!maxmind.validate(clientIp)) {
+      res.status(400).json({ error: "Invalid IP" });
+      return;
+    }
+
+    const location = lookup.get(clientIp);
+    if (!location) {
+      res.status(400).json({ error: "Unknown location" });
+      return;
+    }
+
+    res.json({
+      ip: clientIp,
+      continent: location.continent.names.en,
+      country: location.country.names.en,
+    });
+  });
+
+  app.listen(PORT, () =>
+    console.log(`Server running on port http://localhost:${PORT}`)
+  );
 }
 
-app.use(bodyParser.json());
-
-// ip parametru din path
-/*app.post("/:ip", function (req, res) {
-    maxmind.open('GeoLite2-City_20220802/GeoLite2-City.mmdb').then((lookup) => {
-        let myip = req.params.ip
-        if(maxmind.validate(myip) && lookup.get(myip) != null){
-            location.ip = myip
-            location.continent = lookup.get(myip).continent.names.en
-            location.country = lookup.get(myip).country.names.en
-            res.send(location)  
-            //res.send(`IP: ${myip} \n Continent: ${lookup.get(myip).continent.names.en} \n Country: ${lookup.get(myip).country.names.en}`)
-        }
-        else if (maxmind.validate(myip) === false) {
-            res.send(`invalid IP`)
-            //res.send(`IP: ${myip} \n Continent: ${lookup.get(myip).continent.names.en} \n Country: ${lookup.get(myip).country.names.en}`)
-        }
-        else {
-            res.send(`unknown`)
-        }
-    });    
-})*/
-
-// ip parametru din body
-app.post("/", function (req, res) {
-    maxmind.open('GeoLite2-City_20220802/GeoLite2-City.mmdb').then((lookup) => {
-        let myip = req.body.ip
-        if(maxmind.validate(myip) && lookup.get(myip) != null){
-            location.ip = myip
-            location.continent = lookup.get(myip).continent.names.en
-            location.country = lookup.get(myip).country.names.en
-            res.send(location) 
-        }
-        
-        else if (maxmind.validate(myip) === false) {
-            res.send(`invalid IP`)
-            //res.send(`IP: ${myip} \n Continent: ${lookup.get(myip).continent.names.en} \n Country: ${lookup.get(myip).country.names.en}`)
-        }
-        else {
-            res.send(`unknown`)
-        }
-    });    
-})
-
-
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
+main();
