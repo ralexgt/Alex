e7eb52bb045d1a8b1d9350b423ab4992faf149c5
Alex <romanalexrares@gmail.com>
2022-08-03 16:25:13 +0300

    added index.ts

---
+++ ip-country/index.ts
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const maxmind = require("maxmind");
+
+const PORT = 5000;
+const DB_PATH = "GeoLite2-City_20220802/GeoLite2-City.mmdb";
+
+
+
+async function main(){  
+    const lookup = await maxmind.open(DB_PATH);
+    const app = express();
+    
+    app.use(bodyParser.json());
+    
+    app.post("/api/location", function (req: { body: { ip: string; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string; }): void; new(): any; }; }; json: (arg0: { ip: any; continent: any; country: any; }) => void; }) {
+        const clientIp = req.body.ip
+
+        if(!maxmind.validate(clientIp)){
+            res.status(400).json({ error: "Invalid IP" });
+            return;
+        }
+        
+        interface Location {
+            continent: {
+                names: 
+                    {en: string }
+                },
+            country: {
+                names: 
+                    {en: string }
+                }
+            };
+            
+        const location: Location = lookup.get(clientIp);
+        if(!location) {
+            res.status(400).json({ error: "Unknown location" });
+            return;
+        }
+        
+        res.json({
+            ip: clientIp,
+            continent: location.continent.names.en,
+            country: location.country.names.en
+        });
+    });     
+
+app.listen(PORT, () => 
+    console.log(`Server running on port http://localhost:${PORT}`));
+};
+
+main();
