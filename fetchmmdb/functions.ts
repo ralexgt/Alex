363b57458f5f777f16b399cdb4de13b38f913421
Alex <romanalexrares@gmail.com>
2022-08-08 17:22:05 +0300

    update fetch

--- fetchmmdb/functions.ts
+++ fetchmmdb/functions.ts
@@ -4,31 +4,45 @@ import decompress from "decompress";
 
 
 export function createFile () {
+    return new Promise<void>( async (resolve, reject) => {
     fs.writeFile("fetchedDB.tar.gz", "", function (err) {
     if (err) throw err;
     console.log("File was created successfully. " +  new Date());
-
+    })
+    resolve();
 });
 }
 
 export async function allowsFetch(url: string)
 {
+    return new Promise<void>( async (resolve, reject) => {
     const res = await fetch(url);
-      console.log("download status: " + new Date());
-      console.log(res.ok);
-      console.log(res.status);
+    console.log("download status: " + new Date());
+    console.log(res.ok);
+    console.log(res.status);
+    resolve();
+     })
  }
 
-export async function downloadAndDecompress  (url: string, path: string) {
+export async function downloadArchive  (url: string, path: string) {
+    return new Promise<void>( async (resolve, reject) => {
     const res = await fetch(url);
     const fileStream = fs.createWriteStream(path);
     await new Promise((resolve, reject) => {
         const file = res.body.pipe(fileStream);
         res.body.on("error", reject);
         fileStream.on("finish", resolve);
-    });
-    console.log("archive downloaded " + new Date())
-    decompress(path, "./").then(files => {
-        console.log("files extracted "+ new Date());
-    });
+        console.log("Archive was downloaded " + new Date());
+        })
+    resolve();
+    })
 };
+
+export async function decompressArchive (file: string, destination: string){
+    return new Promise<void>( async (resolve, reject) => {
+    decompress(file, destination).then(files => {
+        console.log(`files extracted to ${destination} `+ new Date());
+    });
+    resolve();
+})
+}
--- fetchmmdb/index.ts
+++ fetchmmdb/index.ts
@@ -1,18 +1,18 @@
 import {allowsFetch} from "./functions";
-import {downloadAndDecompress} from "./functions";
+import {downloadArchive} from "./functions";
 import {createFile} from "./functions";
+import {decompressArchive} from "./functions";
 
-
-function main()
+async function main()
 {
     createFile();
 
     const downloadUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
     const filePath = "./fetchedDB.tar.gz";
 
-    allowsFetch(downloadUrl);
-    downloadAndDecompress(downloadUrl, filePath);
-    
+    await allowsFetch(downloadUrl);
+    await downloadArchive(downloadUrl, filePath);
+    await decompressArchive(filePath, "C:/Users/roman/Desktop/internship_repo/ip-country");   
 }
 
 main()
