ceead4096270a16db74e97bbbfc10d561996078c
Alex <romanalexrares@gmail.com>
2022-08-09 16:47:56 +0300

    tsconfig

--- fetchmmdb/index.ts
+++ fetchmmdb/index.ts
@@ -3,29 +3,24 @@ import { allowsFetch, createFile, downloadArchive } from "./functions";
 
 import fs from "fs";
 
+async function main() {
+  const downloadUrl =
+    "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
+  const filePath = "./fetchedDB.tar.gz";
+  let destination = "./";
 
-async function main()
-{
-    const downloadUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
-    const filePath = "./fetchedDB.tar.gz";
-    let destination = "./"; 
+  if (!fs.existsSync(filePath) || fs.statSync(filePath).size == 0) {
+    await createFile(filePath);
+    await allowsFetch(downloadUrl);
+    await downloadArchive(downloadUrl, filePath);
+  } else {
+    console.log("File path already exists");
+  }
 
-    if(!fs.existsSync(filePath) || fs.statSync(filePath).size == 0) {
-        await createFile(filePath);
-        await allowsFetch(downloadUrl);
-        await downloadArchive(downloadUrl, filePath);
-    }
-        else {
-            console.log("File path already exists");
-        }
-
-    await decompress(filePath, destination).then(() => {
-        if(destination === "./")
-            console.log("files extracted here");
-            else
-            console.log(`files extracted to ${destination} `);
-        
-    })
+  await decompress(filePath, destination).then(() => {
+    if (destination === "./") console.log("files extracted here");
+    else console.log(`files extracted to ${destination} `);
+  });
 }
 
-main()
+main();
