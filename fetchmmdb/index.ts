f7887f4f232b66a1f4e0ddf78f035cefa582371c
Alex <romanalexrares@gmail.com>
2022-08-09 11:54:06 +0300

    fetch update

--- fetchmmdb/index.ts
+++ fetchmmdb/index.ts
@@ -5,13 +5,12 @@ import decompress from "decompress";
 
 import fs from "fs";
 
-
 async function main()
 {
     const downloadUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
     const filePath = "./fetchedDB.tar.gz";
     const destination = "./"; 
-    
+
     if(!fs.existsSync(filePath) || fs.statSync(filePath).size == 0) {
         await createFile(filePath);
         await allowsFetch(downloadUrl);
