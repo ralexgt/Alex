bc6d843b9521e46707e38f0c6f0dc85ab6b6b468
Alex <romanalexrares@gmail.com>
2022-08-10 14:20:41 +0300

    .

--- greetingVariabil/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const express: any;
-declare const bodyParser: any;
-declare const app: any;
-declare const PORT = 3000;
-declare function main(): void;
--- ip-country/scripts/fetch-maxmind-db.ts
+++ ip-country/scripts/fetch-maxmind-db.ts
@@ -1,44 +1,44 @@
 import fs from "fs";
 import path from "path";
 import fetch from "isomorphic-fetch";
-import { mv } from "shelljs";
+import {exec, mv } from "shelljs";
 import decompress from "decompress";
 
 const URL =
-  "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
+	"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=pbBnHIrYZX9EL2ka&suffix=tar.gz";
 
 async function main() {
-  const response = await fetch(URL);
-  if (!response?.body) {
-    throw new Error("No body");
-  }
+	const response = await fetch(URL);
+	if (!response?.body) {
+		throw new Error("No body");
+	}
 
-  const data = await response.arrayBuffer();
+	const data = await response.arrayBuffer();
 
-  const filePath = path.join(process.cwd(), "./GeoLite2-City.tar.gz");
-  await fs.promises.writeFile(filePath, new Uint8Array(data));
+	const filePath = path.join(process.cwd(), "./GeoLite2-City.tar.gz");
+	await fs.promises.writeFile(filePath, new Uint8Array(data));
 
-  console.log("done download");
+	console.log("done download");
 
-  await decompress(filePath, process.cwd());
+	await decompress(filePath, process.cwd());
 
-  console.log("done untar");
+	console.log("done untar");
 
-  await fs.promises.unlink(filePath);
+	await fs.promises.unlink(filePath);
 
-  const files = await fs.promises.readdir(process.cwd());
-  const directoryName = files.find((f) => f.includes("GeoLite2-City"));
+	const files = await fs.promises.readdir(process.cwd());
+	const directoryName = files.find((f) => f.includes("GeoLite2-City"));
 
-  if (!directoryName) {
-    throw new Error("No directory found");
-  }
+	if (!directoryName) {
+		throw new Error("No directory found");
+	}
 
-  mv(
-    path.join(process.cwd(), directoryName),
-    path.join(process.cwd(), "./GeoLite2-City")
-  );
+	mv(
+		path.join(process.cwd(), directoryName),
+		path.join(process.cwd(), "./GeoLite2-City")
+	);
 
-  console.log("done");
+	console.log("done");
 }
-
+  
 main();
